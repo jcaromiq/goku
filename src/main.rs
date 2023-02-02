fff931b44017890db4ac45204a2b4fba7c17df5c
Joaquin Caro <jcaromiq@gmail.com>
2023-02-02 20:42:35 +0100

    with clients and iterations

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,93 @@
 use std::future::Future;
+use std::ops::Div;
 use std::time::Duration;
 use futures::stream::FuturesUnordered;
 use futures::StreamExt;
+use reqwest::{Client, RequestBuilder};
 use tokio::runtime;
 use tokio::runtime::Runtime;
-use tokio::sync::{mpsc, oneshot};
+use tokio::sync::{mpsc, oneshot, Semaphore};
 use tokio::task::JoinHandle;
 use tokio::time::{Instant, sleep};
 
 
 fn main() {
     // let threads = std::cmp::min(num_cpus::get(), config.concurrency as usize);
-    let threads = 1;//std::cmp::min(1, 80 as usize);
-    let rt = runtime::Builder::new_multi_thread().enable_all().worker_threads(threads).build().unwrap();
-    let iterations = 100;
-    let seconds = 0.3;
+    let rt = runtime::Builder::new_multi_thread()
+        .enable_all()
+        .worker_threads(10)
+        .build().unwrap();
+    let iterations = 400;
+    let clients = 400;
     let mut tasks = FuturesUnordered::new();
     rt.block_on(async {
-        by_iteration(&rt, iterations, &mut tasks);
-        // by_time(&rt, seconds, &mut tasks);
+        by_iteration(&rt, clients, iterations, &mut tasks).await;
+        let mut patata: Vec<Vec<R>> = vec![];
         while let Some(finished_task) = tasks.next().await {
             match finished_task {
                 Err(e) => { /* e is a JoinError - the task has panicked */ }
-                Ok(result) => {}
+                Ok(result) => {
+                    patata.push(result);
+                }
             }
         }
+        let patata = patata.into_iter().flatten().collect::<Vec<R>>();
+        let v: f64 = patata
+            .iter()
+            .map(|r| {
+                r.duration
+            }).sum();
+        println!("Total time: {} ms avg {} ", v, v / iterations as f64);
     });
 }
 
-fn by_time(rt: &Runtime, seconds: f64, mut tasks: &mut FuturesUnordered<JoinHandle<R>>) {
-    let begin = Instant::now();
-    let mut x = 0;
-    loop {
-        println!("segundos! {}", begin.elapsed().as_secs_f64());
-        if begin.elapsed().as_secs_f64() > seconds {
-            break;
-        } else {
-            let task = rt.spawn(exec_iterator(x));
-            tasks.push(task);
-            x = x + 1;
-        }
 
+async fn by_iteration(rt: &Runtime, num_clients: u64, iterations: u64, mut tasks: &mut FuturesUnordered<JoinHandle<Vec<R>>>) {
+    let mut clients = Vec::with_capacity(num_clients as usize);
+    for x in 0..num_clients {
+        let client = reqwest::Client::builder().tcp_keepalive(None).build().unwrap();
+        clients.push(client);
     }
-}
+    for (id, c) in clients.into_iter().enumerate() {
+        let task = rt.spawn({
+            exec_iterator(id, iterations / num_clients, c)
+        });
 
-fn by_iteration(rt: &Runtime, iterations: i32, mut tasks: &mut FuturesUnordered<JoinHandle<R>>) {
-    for x in 0..iterations {
-        let task = rt.spawn(exec_iterator(x));
         tasks.push(task);
     }
 }
 
-async fn exec_iterator(iteration: i32) -> R {
-    execute(iteration, 1, "http://localhost:3000/").await;
-    execute(iteration, 2, "http://localhost:3000/users").await;
-    execute(iteration, 3, "http://localhost:3000/").await;
-    execute(iteration, 4, "http://localhost:3000/users").await;
-    R {
-        status: "".to_string(),
-        duration: 0.0,
-        iteration: 0,
+async fn exec_iterator(iteration: usize, num_requests: u64, client: Client) -> Vec<R> {
+    let mut results = vec![];
+    for i in 0..num_requests {
+        let r = exec(iteration, i, &client).await;
+        results.push(r);
+    }
+    results
+}
+
+async fn exec(num_client: usize, execution: u64, client: &Client) -> R {
+    let begin = Instant::now();
+    let response = client.get("http://localhost:3000/users").send().await;
+    let duration_ms = begin.elapsed().as_secs_f64() * 1000.0;
+    println!("Client {}, Execution {} Duration {}", num_client, execution, duration_ms);
+    match response {
+        Ok(r) => {
+            R {
+                status: r.status().to_string(),
+                duration: duration_ms,
+            }
+        }
+        Err(e) => {
+            R {
+                status: "client error".to_string(),
+                duration: duration_ms,
+            }
+        }
     }
 }
 
-async fn execute(iteration: i32, execution: i32, url: &str) -> (Option<String>, f64, ) {
+async fn execute(iteration: usize, execution: i32, url: &str) -> (Option<String>, f64, ) {
     println!("Iteration {}, Execution {}", iteration, execution);
     let begin = Instant::now();
     let response = reqwest::get(url).await;
@@ -78,5 +102,4 @@ async fn execute(iteration: i32, execution: i32, url: &str) -> (Option<String>, 
 struct R {
     status: String,
     duration: f64,
-    iteration: i32,
 }
